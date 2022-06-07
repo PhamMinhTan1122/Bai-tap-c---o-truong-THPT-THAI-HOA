29ee10dd9ec6dfe8f9fe795487a5c670d25ab024
PhamMinhTan1122 <phamtiens289@gmail.com>
2022-06-07 13:35:26 -0700

    them file

---
+++ Bai-tap-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short a,b; //a la chieu dai b la chieu rong
+    printf("Nhap chieu dai: \n");
+    cin >> a;
+    printf("Nhap chieu rong: \n");
+    cin >> b;
+    printf("Chu vi hinh chu nhat la: %d\n", 2*(a+b));
+    printf("Dien tich hinh chu nhat ka: %d\n", a*b);
+
+}
