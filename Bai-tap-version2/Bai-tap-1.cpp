a75975d0322ae2a38baed099b37d775435fefff1
PhamMinhTan1122 <phamtiens289@gmail.com>
2022-06-08 09:54:51 +0700

    hoan thanh 5 bt ver2

---
+++ Bai-tap-version2/Bai-tap-1.cpp
@@ -0,0 +1,20 @@
+//tim so nhat trong 2 So
+#include "C:\Users\Admin\Desktop\Bai-tap-c---o-truong\include.cpp"
+using namespace std;
+int main()
+{
+    int a,b;
+    printf("Nhap so thu nhat: \n");
+    cin >> a;
+    printf("Nhap so thu hai: \n");
+    cin >> b;
+    if(a>b)
+    {
+        printf("So nhat la: %d\n",a);
+    }
+    else
+    {
+        printf("So lon nhat la: %d\n",b);
+    }
+    
+}
---
+++ Bai-tap-version2/Bai-tap-3.cpp
@@ -0,0 +1,34 @@
+
+#include "C:\Users\Admin\Desktop\Bai-tap-c---o-truong\include.cpp"
+int main()
+{
+    // in ra số lớn nhất trong bốn số nhập từ bàn phím bang 2 bien
+    int a, max;
+    printf("Nhap so thu nhat: \n");
+    cin >> a;
+    max = a;
+    if (a > max)
+    {
+        max = a;
+    }
+    printf("Nhap so thu hai: \n");
+    cin >> a;
+    if (a > max)
+    {
+        max = a;
+    }
+    printf("Nhap so thu ba: \n");
+    cin >> a;
+    if (a > max)
+    {
+        max = a;
+    }
+    printf("Nhap so thu tu: \n");
+    cin >> a;
+    if (a > max)
+    {
+        max = a;
+    }
+    printf("kq la: %d", max);
+    return 0;
+}
