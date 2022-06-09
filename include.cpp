33f5639768a495837c245b05ad5e3f98365e6a90
Phạm Minh Tấn <85481444+PhamMinhTan1122@users.noreply.github.com>
2022-06-09 02:46:33 -0700

    include.cpp
    sao no khong chay

--- include.cpp
+++ include.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <string>
 #include <iomanip>
-
+#include <cmath>
 
 
 //dinh nghia hang so
@@ -10,7 +10,7 @@
 #define _CONSTANTS_
 namespace constants
 {
-    const float pi = 3.14; //so pi
+    const float pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679; //so pi
 
 }
 
