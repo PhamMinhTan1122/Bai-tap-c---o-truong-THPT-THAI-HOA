f336dc7158cee078a1e5ee65cb58c6fd8d723c02
PhamMinhTan1122 <phamtiens289@gmail.com>
2022-06-13 14:00:35 -0700

    tim duoc alpha trong c++

--- Bai-tap-4(vong lap)/Bai-tap-15.cpp
+++ Bai-tap-4(vong lap)/Bai-tap-15.cpp
@@ -1,31 +1,24 @@
 #include "../include.cpp"
 int main()
 {
-    int InPut, sum = 0, result,bin;
-    bool DS =  true;
+    int InPut, sum = 0, result;
+    char bin;
     cout << "Moi ban nhap so ban ko thich\n";
     cin >> InPut;
     result = InPut;
-    while (result > 0)
+    while (result >= 0)
     {
         if (result % 2 == 0)
         {
-            DS = true;
+            bin = '0';
         }
         else 
         {
-            DS = false;
+            bin = '1';
         }
-        result = InPut/2;
-        if(DS = true)
-        {
-            bin = 0;
-        }
-        else 
-        {
-            bin = 1;
-        }
-        bin = bin * 10;
+        result = result/2;
+        bin = bin *10;
+        cout << bin;
         return result;
     }
     cout << bin << endl;
