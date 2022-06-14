28c99f012973178b133fe9cf3fea678c2d4e4886
PhamMinhTan1122 <phamtiens289@gmail.com>
2022-06-14 07:56:37 -0700

    loi gi

--- Bai-tap-4(vong lap)/Bai-tap-5.cpp
+++
@@ -1,9 +0,0 @@
-// Hãy in ra mã ASCII của các kí tự từ “A” đến “Z” và từ “a” đến “z”
-#include "../include.cpp"//include các thư viện cần thiết
-int main()
-{
-    for(char i = 'A'; i <= 'z'; i++)
-    {
-        printf("%d\t %c\n",(int)i,i);
-    }
-}
