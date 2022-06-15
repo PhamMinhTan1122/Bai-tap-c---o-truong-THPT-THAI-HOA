d49f90318aa2bd8d1ceee7a5f60499de23d4b014
PhamMinhTan1122 <phamtiens289@gmail.com>
2022-06-15 07:45:40 -0700

    da xong bai bt 5

--- Bai-tap-version6/Bai-tap-5.cpp
+++ Bai-tap-version6/Bai-tap-5.cpp
@@ -1,18 +1,17 @@
 #include "../include.cpp"
 int main()
 {
-    float result, Cash;
+    float result, Cash, lai;
     float Count = 0, Cash_1;
     cout << "Nhap so tien gui vao ngan hang:";
     cin >> Cash;
     cout << "Nhap so tien can rut tien: ";
     cin >> Cash_1;
-    while (Cash_1 >= Cash)
+    lai = Cash*(0.3/100);
+    while (Cash_1 > Cash)
     {
-        result = Cash * 0.3 / 100;
-
-        // printf("So tien %.f sao %.f thang la %.f\n ", Cash, Count, result);
+        Cash = Cash + lai;
         Count++;
     }
-    cout << "So tien " << Cash << " sau " << Count << " thang la " << Cash_1 << endl;
+    cout << "So tien " << Cash << " sau " << Count << " thang la " << Cash << endl; 
 }
